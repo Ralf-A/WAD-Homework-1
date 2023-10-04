da562dbdc90952bd46084d1c472032682691831b
OPuusalu <opuusalu@gmail.com>
2023-10-04 15:46:54 +0300

    epic commit 9

--- postScript.js
+++ postScript.js
@@ -5,17 +5,21 @@ const postsContainer = document.getElementById('posts-container');
 fetch('/res/data.json') // Replace 'your-json-file.json' with the actual path to your JSON file
   .then(response => response.json())
   .then(data => {
+    // Create an unordered list for the posts
+    const postsList = document.createElement('ul');
+    postsList.classList.add('post-list'); // Add a class for styling the list
+
     // Loop through the posts and display them
     data.posts.forEach(post => {
-      const postElement = document.createElement('div');
-      postElement.classList.add('post-outer-container'); // Add a class for outer div styling
+      // Create a list item for each post
+      const listItem = document.createElement('dl');
+      listItem.classList.add('post-item'); // Add a class for styling the list item
 
       // Create a div for the date (top-right)
       const dateElement = document.createElement('p');
       dateElement.textContent = `Date: ${post.date}`;
       dateElement.classList.add('date-top-right'); // Add styling class for positioning the date
-      postElement.appendChild(dateElement);
-
+      listItem.appendChild(dateElement);
       // Check if there's an image URL provided
       if (post.picture) {
         const imageElement = document.createElement('img');
@@ -23,22 +27,21 @@ fetch('/res/data.json') // Replace 'your-json-file.json' with the actual path to
         imageElement.alt = `Image for Post ${post.ID}`;
         imageElement.style.width = '100%'; // Set width to 100%
         imageElement.style.height = 'auto'; // Maintain aspect ratio
-        postElement.appendChild(imageElement);
-      } else {
-        const emptyElement = document.createElement('p');
-        postElement.appendChild(emptyElement);
+        listItem.appendChild(imageElement);
       }
 
-      // Add the date text before the content text
-
+      // Add the date and content text to the list item
 
       // Add the content text
       const contentElement = document.createElement('p');
       contentElement.textContent = post.content;
-      postElement.appendChild(contentElement);
+      listItem.appendChild(contentElement);
 
-      // Append the post div to the posts container
-      postsContainer.appendChild(postElement);
+      // Add the list item to the posts list
+      postsList.appendChild(listItem);
     });
+
+    // Append the posts list to the posts container
+    postsContainer.appendChild(postsList);
   })
   .catch(error => console.error('Error fetching and displaying posts:', error));
