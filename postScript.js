0a451a27196dde0d04514fea09ec85a535dd8650
Ralf-A <ralfanimagi@gmail.com>
2023-11-05 00:29:09 +0200

    Posts now fetched from API endpoint
    JSON file with 10 posts, validated it, endpoint on github gist, fetches and updates page with it, stores both pictures and text

--- postScript.js
+++ postScript.js
@@ -1,8 +1,8 @@
 // Select the posts container div
 const postsContainer = document.getElementById('posts-container');
 
-// Fetch the JSON file
-fetch('/res/data.json') // Replace 'your-json-file.json' with the actual path to your JSON file
+// Fetch the JSON file from gist 
+fetch('https://gist.githubusercontent.com/Ralf-A/bd247c4126a4af73edb3fd21c4e8e22f/raw/data.json')
   .then(response => response.json())
   .then(data => {
     // Create an unordered list for the posts
@@ -24,7 +24,7 @@ fetch('/res/data.json') // Replace 'your-json-file.json' with the actual path to
       // Check if there's an image URL provided
       if (post.picture) {
         const imageElement = document.createElement('img');
-        imageElement.src = `res/pictures/${post.picture}`;
+        imageElement.src = `${post.picture}`;
         imageElement.alt = `Image for Post ${post.ID}`;
         imageElement.style.width = '100%'; // Set width to 100%
         imageElement.style.height = 'auto'; // Maintain aspect ratio
@@ -46,3 +46,4 @@ fetch('/res/data.json') // Replace 'your-json-file.json' with the actual path to
     postsContainer.appendChild(postsList);
   })
   .catch(error => console.error('Error fetching and displaying posts:', error));
+  
